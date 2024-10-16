1314f3b1bf19022767120d9f7342b4cf1531b6be
RutujaPachore123 <rutuja3575@gmail.com>
2024-10-16 09:42:03 -0800

    Update Pharma Sales_Query.sql

--- Pharma Sales_Query.sql.sql
+++ Pharma Sales_Query.sql.sql
@@ -3,12 +3,14 @@
 SELECT SUM(Totalamount) AS TotalSales 
 FROM Sales;
 
+
 -- Q2.What is the current stock level for each product?
 
 SELECT P.ProductName, i.QuantityInStock
 FROM Products AS P 
 JOIN Inventory AS i 
 ON P.ProductID = i.ProductID;
+
 
 -- Q3.Which product has generated the highest sales amount?
 
@@ -19,6 +21,7 @@ GROUP BY p.ProductName, s.ProductID
 ORDER BY TotalSales DESC 
 LIMIT 1;
 
+
 -- Q4.Which suppliers provide a specific product (e.g., ProductID = 101)?
 
 SELECT s.SupplierName 
@@ -26,12 +29,14 @@ FROM Suppliers s
 JOIN Products p ON s.ProductID = p.ProductID 
 WHERE p.ProductID = 101;
 
+
 -- Q5.What are the contact details of customers who have made purchases?
 
 SELECT DISTINCT c.CustomerID, c.CustomerName, c.ContactNumber, c.Email
 FROM Customers c
 JOIN Sales s 
 ON c.CustomerID = s.CustomerID;
+
 
 -- Q6.Which products are running low on stock (e.g., less than 50)?
 
@@ -41,6 +46,7 @@ JOIN Inventory i
 ON p.ProductID = i.ProductID
 WHERE i.QuantityInStock < 50;
 
+
 -- Q7.What is the total sales amount by product category?
 
 SELECT p.Category, SUM(s.TotalAmount) AS TotalSales
@@ -49,6 +55,7 @@ JOIN Products p
 ON s.ProductID = p.ProductID 
 GROUP BY p.Category
 ORDER BY TotalSales DESC;
+
 
 -- Q8.Which customers have spent the most money?
 
@@ -60,11 +67,13 @@ GROUP BY c.CustomerName
 ORDER BY TotalSpent DESC
 LIMIT 5;
 
+
 -- Q9.What were the total sales on a particular date (e.g., '2024-10-1')?
 
 SELECT SUM(TotalAmount) AS TotalSales
 FROM Sales
 WHERE SaleDate = '2024-10-01';
+
 
 -- Q10.Which customers are from a specific location (e.g., 'Mumbai')?
 
